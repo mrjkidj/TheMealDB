2857c6f71a514477515683ebe7ed9cd6fb892aaa
shaabolot <shaabolotamatov@gmail.com>
2024-02-19 10:35:27 +0600

    themealdb

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Main from './Pages/Main';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
